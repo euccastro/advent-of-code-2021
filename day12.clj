dec21ae390c311e98b56eeb0e27137b2390265e5
Estevo U. C. Castro <euccastro@gmail.com>
2021-12-12 20:27:42 +0100

    day 12

--- day12.clj
+++ day12.clj
@@ -1,13 +1,7 @@
 (ns day12
   (:require [clojure.string :as str]))
 
-(def input "start-A
-start-b
-A-c
-A-b
-b-d
-A-end
-b-end")
+(def input (slurp "input12"))
 
 (defn map-vals [f m]
   (into {}
@@ -21,4 +15,32 @@ b-end")
                 (group-by first)
                 (map-vals (partial mapv second))))
 
-;; {"start" ["A" "b"], "A" ["c" "b" "end"], "b" ["d" "end"]}
+(defn count-paths [node visited]
+  (if (= node "end")
+    1
+    (let [visited (cond-> visited
+                    (not= node (str/upper-case node)) (conj node))]
+      (reduce +
+              0
+              (map #(count-paths % visited)
+                   (remove visited (graph node)))))))
+
+(count-paths "start" #{})
+
+(defn count-paths2 [node visited allow-revisit?]
+  (if (= node "end")
+    1
+    (let [small? (not= node (str/upper-case node))
+          allow-revisit? (and allow-revisit?
+                              (not (and small? (visited node))))
+          visited (cond-> visited
+                    small? (conj node))]
+      (reduce +
+              0
+              (map #(count-paths2 % visited allow-revisit?)
+                   (cond->>
+                       (remove #{"start"} (graph node))
+                     (not allow-revisit?) (remove visited)))))))
+
+(count-paths2 "start" #{} true)
+;; => 146553
